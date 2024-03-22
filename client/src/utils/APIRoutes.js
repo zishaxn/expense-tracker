c5d38c9141caabef647f2cab8ca0f83a1c3b4aa9
zishan chaudhary <zishan8291@gmail.com>
2024-03-23 02:17:59 +0530

    fix host links in API

--- client/src/utils/APIRoutes.js
+++ client/src/utils/APIRoutes.js
@@ -1,6 +1,8 @@
-export const host = "http://localhost:5000";
+export const host =
+  "https://expense-tracker-2u16isg2y-zishans-projects.vercel.app";
 
-export const getTransactions = `${host}/api/v1/transactions`;
-export const addTransactions = `${host}/api/v1/transactions`;
+export const getTransactions = `https://expense-tracker-2u16isg2y-zishans-projects.vercel.app/api/v1/transactions`;
+export const addTransactions = `http://localhost:5000//api/v1/transactions`;
 // export const deleteTransactions = `${host}/api/v1/transactions/:`;
-export const deleteTransaction = (id) => `${host}/api/v1/transactions/${id}`;
+export const deleteTransaction = (id) =>
+  `http://localhost:5000//api/v1/transactions/${id}`;
